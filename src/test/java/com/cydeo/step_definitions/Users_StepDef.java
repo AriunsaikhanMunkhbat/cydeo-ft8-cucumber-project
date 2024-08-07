c68eab89be18655d0acf02b681dde40f45e1c9e6
adamar <aysun114@hotmail.com>
2024-08-06 19:05:50 -0500

    Users_StepDef added

--- src/test/java/com/cydeo/step_definitions/Users_StepDef.java
+++ src/test/java/com/cydeo/step_definitions/Users_StepDef.java
@@ -1,24 +1,36 @@
 package com.cydeo.step_definitions;
 
+import com.cydeo.pages.UsersPage;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+import org.junit.Assert;
+import org.openqa.selenium.WebElement;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Users_StepDef {
 
+    UsersPage usersPage = new UsersPage();
+
     @Given("I click on {string} link")
     public void i_click_on_link(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
+        usersPage.usersLink.click();
+
     }
+
     @Then("table should have following column names:")
-    public void table_should_have_following_column_names(io.cucumber.datatable.DataTable dataTable) {
-        // Write code here that turns the phrase above into concrete actions
-        // For automatic transformation, change DataTable to one of
-        // E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
-        // Map<K, List<V>>. E,K,V must be a String, Integer, Float,
-        // Double, Byte, Short, Long, BigInteger or BigDecimal.
-        //
-        // For other transformations you can register a DataTableType.
-        throw new io.cucumber.java.PendingException();
+    public void table_should_have_following_column_names(List<String> expectedHeaders) {
+
+        List<String> actualHeaders = new ArrayList<>();
+
+        for (WebElement eachHeader : usersPage.tableHeaders) {
+            actualHeaders.add(eachHeader.getText());
+        }
+
+        Assert.assertEquals(expectedHeaders,actualHeaders);
+
+
     }
 }
